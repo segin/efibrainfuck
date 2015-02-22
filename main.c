a0fd6b45c637e8400005a642dcf373470396ce53
Kirn Gill <segin2005@gmail.com>
2015-02-22 01:55:17 -0600

    Added init code

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include <efi.h>
 #include <efilib.h>
 
+static void
+usage(void)
+{
+   Print(L"Usage: efibrainfuck code.bf\n");
+}
+
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+   EFI_STATUS status;
+   UINTN argc;
+   CHAR16 **argv;
    InitializeLib(ImageHandle, SystemTable);
-   Print(L"Peter can't Cthulhu this.\n");
-   Print(L"\nSTOP.\n\nHammertime.\n");
-
+   status = get_args(ImageHandle, &argc, &argv);
+   if (EFI_ERROR(status)) { 
+     Print(L"ERROR: Parsing command line arguments: %d\n", status); 
+     return status; 
+   }
+   
+   
+   
    return EFI_SUCCESS;
 }
