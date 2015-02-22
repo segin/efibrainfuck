9e75e76cc65615d473088b919f49789f5673feab
Kirn Gill <segin2005@gmail.com>
2015-02-22 02:01:58 -0600

    Added usage info.

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 static void
 usage(void)
 {
-   Print(L"Usage: efibrainfuck code.bf\n");
+   Print(L"Usage: efibrainfuck code.bf [input.txt]\n");
+   Print(L"If no input text provided, input will read zeros.\n");
 }
 
 EFI_STATUS
@@ -19,7 +20,10 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
      Print(L"ERROR: Parsing command line arguments: %d\n", status); 
      return status; 
    }
-   
+   if (argc < 2) {
+     usage();
+     return EFI_SUCCESS;
+   }
    
    
    return EFI_SUCCESS;
