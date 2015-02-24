c267982a6895bdba065811a8321c0c16c5f9ec1d
Kirn Gill <segin2005@gmail.com>
2015-02-24 09:58:49 -0600

    Added malloc implementation

---
+++ malloc.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2011, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "efibrainfuck.h"
+
+/**
+ * malloc - Allocate memory from the EfiLoaderData pool
+ * @size: size in bytes of the requested allocation
+ *
+ * Return a pointer to an allocation of @size bytes of type
+ * EfiLoaderData.
+ */
+void *malloc(UINTN size)
+{
+	EFI_STATUS err;
+	void *buffer;
+
+	err = allocate_pool(EfiLoaderData, size, &buffer);
+	if (err != EFI_SUCCESS)
+		buffer = NULL;
+
+	return buffer;
+}
+
+/**
+ * free - Release memory to the EfiLoaderData pool
+ * @buffer: pointer to the malloc() allocation to free
+ */
+void free(void *buffer)
+{
+	free_pool(buffer);
+}
---
+++ malloc.h
@@ -0,0 +1,38 @@
+/* EFI malloc support library */
+
+#ifndef __EFI_MALLOC_H__
+#define __EFI_MALLOC_H__
+
+/**
+ * allocate_pool - Allocate pool memory
+ * @type: the type of pool to allocate
+ * @size: number of bytes to allocate from pool of @type
+ * @buffer: used to return the address of allocated memory
+ *
+ * Allocate memory from pool of @type. If the pool needs more memory
+ * pages are allocated from EfiConventionalMemory in order to grow the
+ * pool.
+ *
+ * All allocations are eight-byte aligned.
+ */
+static inline EFI_STATUS
+allocate_pool(EFI_MEMORY_TYPE type, UINTN size, void **buffer)
+{
+	return uefi_call_wrapper(boot->AllocatePool, 3, type, size, buffer);
+}
+
+/**
+ * free_pool - Return pool memory to the system
+ * @buffer: the buffer to free
+ *
+ * Return @buffer to the system. The returned memory is marked as
+ * EfiConventionalMemory.
+ */
+static inline EFI_STATUS free_pool(void *buffer)
+{
+	return uefi_call_wrapper(boot->FreePool, 1, buffer);
+}
+
+
+
+#endif /* __EFI_MALLOC_H__ */
