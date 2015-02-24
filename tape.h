629f19e47d7d33032723b79a30e18bcded0086a4
Kirn Gill <segin2005@gmail.com>
2015-02-24 11:19:46 -0600

    Added function definitions

--- tape.h
+++ tape.h
@@ -5,9 +5,20 @@
 
 #define TAPESIZE 0x10000
 
+/* Types */
 struct bf_tape { 
 	UINTN position;
 	char  tape[TAPESIZE];
 };
+
+/* Functions */
+struct bf_tape *bf_tape_init(void);
+void bf_tape_fini(struct bf_tape *this);
+char bf_tape_get(struct bf_tape *this);
+void bf_tape_set(struct bf_tape *this, char value);
+void bf_tape_inc(struct bf_tape *this);
+void bf_tape_dec(struct bf_tape *this);
+void bf_tape_forward(struct bf_tape *this);
+void bf_tape_reverse(struct bf_tape *this);
 
 #endif /* __TAPE_H__ */
