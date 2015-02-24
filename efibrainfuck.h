500dc04daa39258bf8158e4086f38e5c822d38ae
Kirn Gill <segin2005@gmail.com>
2015-02-24 09:58:28 -0600

    Added master header

---
+++ efibrainfuck.h
@@ -0,0 +1,14 @@
+/* EFI Brainfuck */
+
+#ifndef __EFIBRAINFUCK_H__
+#define __EFIBRAINFUCK_H__
+
+/* system includes */
+#include <efi.h>
+#include <efilib.h>
+
+/* local includes */ 
+#include "malloc.h"
+#include "tape.h"
+
+#endif /* __EFIBRAINFUCK_H__ */ 
