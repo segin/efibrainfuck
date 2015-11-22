d36792ee6ea30779be92fcb377322d4b94e2c640
Kirn Gill <segin2005@gmail.com>
2015-11-21 22:51:06 -0600

    Added ShellParameters protocol related entries.

--- efibrainfuck.h
+++ efibrainfuck.h
@@ -20,4 +20,19 @@ UINTN AsciiStrLen(IN CHAR8 *s1);
 #include "tape.h"
 #include "interpreter.h"
 
+/* Portability macros */
+#ifndef EFI_SHELL_PARAMETERS_PROTOCOL_GUID
+#define EFI_SHELL_PARAMETERS_PROTOCOL_GUID \
+	{ 0x752f3136, 0x4e16, 0x4fdc, \
+	{ 0xa2, 0x2a, 0xe5, 0xf4, 0x68, 0x12, 0xf4, 0xca } }; 
+
+	typedef struct _EFI_SHELL_PARAMETERS_PROTOCOL {
+		 CHAR16 **Argv;
+		 UINTN Argc;
+		 EFI_FILE_HANDLE StdIn;
+		 EFI_FILE_HANDLE StdOut;
+		 EFI_FILE_HANDLE StdErr;
+	} EFI_SHELL_PARAMETERS_PROTOCOL; 
+#endif /* EFI_SHELL_PARAMETERS_PROTOCOL_GUID */
+
 #endif /* __EFIBRAINFUCK_H__ */ 
